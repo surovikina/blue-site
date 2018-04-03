f85e0d0d754fa90e1e59d9e3fa1432f4a3fd957f
surovikina <korol0955559531@gmail.com>
2018-04-04 00:16:15 +0300

    Sliders was done

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,23 @@
-$(document).ready(function(){
-  $(".owl-carousel").owlCarousel();
+
+  $('.sl').slick({
+    slidesToShow: 3
+    // centerMode: true;
+    //centerPadding: 10.5%;
+  });
+
+
+  $('.slider').slick({
+ slidesToShow: 1,
+ slidesToScroll: 1,
+ arrows: true,
+ fade: false,
+ asNavFor: '.slider-nav-thumbnails',
+});
+
+$('.slider-nav-thumbnails').slick({
+ slidesToShow: 5,
+ slidesToScroll: 1,
+ asNavFor: '.slider',
+ dots: true,
+ focusOnSelect: true
 });
